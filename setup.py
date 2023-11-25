549c62beee6794f7b99ee967725fc1c3e5d9098d
dkunyk <d.kunyk@quantumobile.com>
2023-11-25 17:46:18 +0200

    update deps

--- setup.py
+++ setup.py
@@ -2,10 +2,21 @@
 from setuptools import setup
 
 packages = \
-['dlu_core']
+['dlu_core', 'dlu_core.object_detection', 'dlu_core.object_detection.yolo']
 
 package_data = \
 {'': ['*']}
+
+install_requires = \
+['albumentations>=1.3.1,<2.0.0',
+ 'black>=23.11.0,<24.0.0',
+ 'hydra-core>=1.3.2,<2.0.0',
+ 'isort>=5.12.0,<6.0.0',
+ 'matplotlib>=3.8.2,<4.0.0',
+ 'numpy>=1.26.2,<2.0.0',
+ 'pandas>=2.1.3,<3.0.0',
+ 'pylint>=3.0.2,<4.0.0',
+ 'tqdm>=4.66.1,<5.0.0']
 
 setup_kwargs = {
     'name': 'dlu_core',
@@ -19,6 +30,7 @@ setup_kwargs = {
     'url': 'None',
     'packages': packages,
     'package_data': package_data,
+    'install_requires': install_requires,
     'python_requires': '>=3.9,<4.0',
 }
 
